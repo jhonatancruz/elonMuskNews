68163f8ef0b7db04c1a408d6a54ef061732b4868
Jhonatan Cruz <jcruz3@drew.edu>
2018-03-13 12:47:46 -0400

    added scheduling functionality for hourly refreshing of news

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 from flask import Flask, render_template
 from getNews import *
+import schedule, time
+from threading import Thread
 
 app= Flask(__name__)
 
 
-@app.route("/")
-def main():
+def refreshNews():
+    global allNews, allTesla, allspaceX, allboringCompany
+    #all elon musk news
     elon= elonMuskNews("elon musk")
     elon.getData()
     allNews= elon.getEverything()
-    # headline=elon.getHeadline(1)
-    # url= elon.getUrl(1)
-    # imgUrl= elon.getImgUrls(1)
-    # providedBy= elon.getProvidedBy(1)
+
+    # all tesla news
+    tesla= elonMuskNews("tesla")
+    tesla.getData()
+    allTesla= tesla.getEverything()
+
+    #all spacex news
+    spaceX= elonMuskNews("spacex")
+    spaceX.getData()
+    allspaceX= spaceX.getEverything()
+
+    #all boring company news
+    boringCompany= elonMuskNews("elon musk")
+    boringCompany.getData()
+    allboringCompany= boringCompany.getEverything()
+
+def run_schedule():
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+@app.route("/")
+def main():
     print("finished")
-    return render_template("home.html", allNews=allNews)
+    return render_template("home.html", allNews=allNews, allTesla=allTesla, allspaceX=allspaceX,
+                            allboringCompany=allboringCompany)
 
-# def retrieveData():
-#     elon= elonMuskNews("elon musk")
-#     elon.getData()
-#     everything= elon.getEverything()
-#     # position= input()
-#     # count=0
-#     # for x in everything:
-#     #     count+=1
-#     #
-#     print(everything["headlines"][0])
-#
-# retrieveData()
-
+refreshNews()
 if __name__== "__main__":
+    schedule.every(1).minutes.do(refreshNews)
+    t = Thread(target=run_schedule)
+    t.start()
     app.run(debug=True)
